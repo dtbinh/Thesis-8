4de4f8c9815693fc9889664600574634fc389366
Seyedmajid Azimi Gehraz <seyedmajid.azimi.gehraz@sap.com>
2018-06-18 16:34:36 +0200

    Change BatchListener to abstract class

--- Code/spark-rl-resource-manager/src/main/scala/org/apache/spark/streaming/scheduler/BatchListener.scala
+++ Code/spark-rl-resource-manager/src/main/scala/org/apache/spark/streaming/scheduler/BatchListener.scala
@@ -3,7 +3,7 @@ package org.apache.spark.streaming.scheduler
 import com.sap.rl.window.Window
 import org.apache.spark.internal.Logging
 
-class BatchListener(private val window: Window) extends StreamingListener with Logging {
+abstract class BatchListener(val window: Window) extends StreamingListener with Logging {
 
   override def onBatchCompleted(batchCompleted: StreamingListenerBatchCompleted): Unit = {
     val totalDelay: Long = batchCompleted.batchInfo.totalDelay.get
