42584cb64baee248d6c755dbffcfed1ff50c0ac3
Majid Azimi <majid.azimi@live.com>
2018-07-08 17:45:58 +0200

    better reward function, needs to be improved

--- Code/spark-rl-resource-manager/src/main/scala/com/sap/rl/rm/impl/DefaultReward.scala
+++ Code/spark-rl-resource-manager/src/main/scala/com/sap/rl/rm/impl/DefaultReward.scala
@@ -2,34 +2,32 @@ package com.sap.rl.rm.impl
 
 import com.sap.rl.rm.Action._
 import com.sap.rl.rm.{RMConstants, Reward, State, StateSpace}
+import org.apache.log4j.LogManager
 
 class DefaultReward(constants: RMConstants, stateSpace: StateSpace) extends Reward {
 
   import constants._
 
-  // TODO: fix this shit, consider incoming messages as well
+  @transient private lazy val log = LogManager.getLogger(this.getClass)
+
   override def forAction(lastState: State, lastAction: Action, currentState: State): Double = {
     if (isInDangerZone(lastState, currentState)) {
-      if (lastAction == ScaleOut)
+      if (lastAction == ScaleOut) {
+        log.info("")
         BestReward
-      // if we are in danger zone, no matter which action to take, it is really bad
+      }
       else
+        // if we are in danger zone, no matter which action to take, it is still bad.
         RewardMultiplier * (CoarseTargetLatency - currentState.latency).toDouble / currentState.latency
     } else {
-      // if we are in safe zone, ScaleOut is the worst possible action
-      if (lastAction == ScaleOut)
-        -RewardMultiplier * BestReward
       // if we are in absolute safe zone, ScaleIn has the best reward
-      else if (isInAbsoluteSafeZone(lastState, currentState) && lastAction == ScaleIn)
-        BestReward
-      // if both states are in yellow zone, NoAction is better. Don't mess with cluster
-      else if (isBothStatesInYellowZone(lastState, currentState) && lastAction == NoAction)
-        BestReward
-      // if load is increasing, and we are in yellow zone, NoAction is better than ScaleIn
-      else if (isLoadIncreasing(lastState, currentState) && isCurrentStateInYellowZone(currentState) && lastAction == NoAction)
-
-      // if incoming messages are increasing, and we are not in yellow zone, ScaleIn is better
-      else if (currentState.incomingMessages > lastState.incomingMessages && lastAction == ScaleIn) -BestReward / currentState.numberOfExecutors
+      if (isInAbsoluteSafeZone(lastState, currentState) && lastAction == ScaleIn) BestReward
+      // prefer scale-out when we are in danger zone
+      else if (isStateInYellowZone(currentState) && isLoadIncreasing(lastState, currentState))
+        if (lastAction == ScaleOut) BestReward
+        else -BestReward
+      else if (lastAction == ScaleOut) -BestReward
+      else if (isLoadDecreasing(lastState, currentState) && lastAction == NoAction) -BestReward
       else BestReward / currentState.numberOfExecutors
     }
   }
@@ -40,19 +38,16 @@ class DefaultReward(constants: RMConstants, stateSpace: StateSpace) extends Rewa
   def isInAbsoluteSafeZone(lastState: State, currentState: State): Boolean =
     lastState.latency < CoarseMinimumLatency && currentState.latency < CoarseMinimumLatency
 
-  def isBothStatesInYellowZone(lastState: State, currentState: State): Boolean = {
-    val threshold = CoarseTargetLatency - LatencyYellowZoneSteps
-    lastState.latency >= threshold && currentState.latency >= threshold
-  }
+  def isBothStatesInYellowZone(lastState: State, currentState: State): Boolean =
+    isStateInYellowZone(lastState) && isStateInYellowZone(currentState)
 
-  def isCurrentStateInYellowZone(currentState: State): Boolean =
-    currentState.latency >= CoarseTargetLatency - LatencyYellowZoneSteps
-
-  def isLoadDecreasing(lastState: State, currentState: State): Boolean =
-    !isLoadIncreasing(lastState, currentState)
+  def isStateInYellowZone(s: State): Boolean = s.latency >= CoarseTargetLatency - LatencyYellowZoneSteps
 
   def isLoadIncreasing(lastState: State, currentState: State): Boolean =
     currentState.incomingMessages > lastState.incomingMessages
+
+  def isLoadDecreasing(lastState: State, currentState: State): Boolean =
+    currentState.incomingMessages < lastState.incomingMessages
 }
 
 object DefaultReward {
