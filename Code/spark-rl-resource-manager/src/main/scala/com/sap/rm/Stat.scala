5a004d2d0d928981a9de55c6779ba1d6c87f385f
Seyedmajid Azimi Gehraz <seyedmajid.azimi.gehraz@sap.com>
2018-08-10 16:48:49 +0200

    Fix minor change in log message

--- Code/spark-rl-resource-manager/src/main/scala/com/sap/rm/Stat.scala
+++ Code/spark-rl-resource-manager/src/main/scala/com/sap/rm/Stat.scala
@@ -6,7 +6,7 @@ case class Stat (TotalBatches: Int, TotalSLOViolations: Int,
                  AverageProcessingTime: Int, AverageTotalDelay: Int, Executors: Int,
                  WindowID: Int, WindowSize: Int, WindowSLOViolations: Int) {
   override def toString: String = {
-    "TotalBatches=%d,TotalSLOViolations=%d,AverageLatency=%d,AverageTotalDelay=%d,Executors=%d,WindowID=%d,WindowSize=%d,WindowSLOViolations=%d"
+    "TotalBatches=%d,TotalSLOViolations=%d,AverageProcessingTime=%d,AverageTotalDelay=%d,Executors=%d,WindowID=%d,WindowSize=%d,WindowSLOViolations=%d"
       .format(TotalBatches,TotalSLOViolations,AverageProcessingTime,AverageTotalDelay,Executors,WindowID,WindowSize,WindowSLOViolations)
   }
 }
