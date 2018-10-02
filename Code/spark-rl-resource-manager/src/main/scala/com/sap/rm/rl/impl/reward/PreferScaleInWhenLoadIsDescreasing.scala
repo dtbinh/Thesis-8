ab7e20a375c28ba8a800cd854b51332ceb145bb8
Seyedmajid Azimi Gehraz <seyedmajid.azimi.gehraz@sap.com>
2018-10-02 18:47:17 +0200

    MInor Improvement for reward function

--- Code/spark-rl-resource-manager/src/main/scala/com/sap/rm/rl/impl/reward/PreferScaleInWhenLoadIsDescreasing.scala
+++ Code/spark-rl-resource-manager/src/main/scala/com/sap/rm/rl/impl/reward/PreferScaleInWhenLoadIsDescreasing.scala
@@ -9,6 +9,8 @@ import com.sap.rm.rl.{BatchWaitingList, Reward, State, StateSpace}
 
 class PreferScaleInWhenLoadIsDescreasing(val config: ResourceManagerConfig) extends Reward {
 
+  import config._
+
   override def forAction(stateSpace: StateSpace, lastState: State, lastAction: Action, currentState: State, waitingList: BatchWaitingList, numberOfExecutors: Int): Option[Double] = {
     val partialReward: Option[Double] = super.forAction(stateSpace, lastState, lastAction, currentState, waitingList, numberOfExecutors)
     if (partialReward.nonEmpty) {
@@ -19,7 +21,7 @@ class PreferScaleInWhenLoadIsDescreasing(val config: ResourceManagerConfig) exte
     val ratio = executorRatio(numberOfExecutors)
 
     // current is not in danger zone, now check for last state
-    if (!isStateInDangerZone(lastState) && lastAction == NoAction && !currentState.loadIsIncreasing) {
+    if (!isStateInDangerZone(lastState) && lastAction == NoAction && !currentState.loadIsIncreasing && numberOfExecutors != MinimumExecutors) {
       // if latency is decreasing, then no action gets negative reward
       if ((currentState.latency < lastState.latency) || (currentState.latency == lastState.latency && currentState.latency == 0)) {
         return Some(-ratio)
